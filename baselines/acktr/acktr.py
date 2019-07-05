13e14e3ed43ad8ddbfd87de468811a89c286b303
Moritz <moritz.schneider@live.de>
2019-07-05 23:21:14 +0200

    Added defaults and kfac_utils

---
+++ baselines/acktr/acktr.py
@@ -0,0 +1,8 @@
+
+
+class Model(object):
+
+    def __init__(self, policy, ob_space, ac_space, nenvs,total_timesteps, nprocs=32, nsteps=20,
+                 ent_coef=0.01, vf_coef=0.5, vf_fisher_coef=1.0, lr=0.25, max_grad_norm=0.5,
+                 kfac_clip=0.001, lrschedule='linear', is_async=True):
+        pass
